330527ab7b8cbe9460725d3bdaa11fd226aa5831
yangxiongbro <yangxiongbro@163.com>
2023-12-21 23:05:43 +0800

    edit:更新

--- src/main/java/com/common/auth/controller/HelloWorldController.java
+++ src/main/java/com/common/auth/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.common.auth.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * <b><code>HelloworldController</code></b>
@@ -14,13 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yang xiong
  * @since CommonAuth 1.0
  */
-@RestController
+@Controller
 @RequestMapping("/hello_world")
 public class HelloWorldController {
 
     @GetMapping
+    @ResponseBody
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    @GetMapping("/demo")
+    public String demo(Map<String,Object> map){
+        map.put("name", "Lily");
+        return "demo";
     }
 
 }
